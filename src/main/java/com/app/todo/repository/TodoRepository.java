bb41ef7b48f8d16424c39b49dcbce50f3dcdc1a6
TassioTristao <tassio.tristao05@gmail.com>
2025-06-04 08:23:48 -0300

    feat(repository): create JPA repositories for data access

---
+++ src/main/java/com/app/todo/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.app.todo.repository;
+
+import com.app.todo.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
